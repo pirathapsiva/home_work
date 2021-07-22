b3257d78fbd24d63f9a3d9f207dbf2492b39c435
pirathapsiva <sspirathap26370@gmail.com>
2021-07-22 20:51:44 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,7 @@
+function cricle (x,y,z) {
+  w = x*y*z
+};
+cricle(2,22/7,4);
+
+
+alert("w");
---
+++ java_sc.js
@@ -0,0 +1,29 @@
+console.log(5 + 6);
+alert("hiii hello what are doing");
+
+/*
+let x, y, z;
+x = 6;
+y = 9;
+z = x + y;
+document.getElementById("cat").innerHTML = "The value" + z + ".";
+*/
+
+
+function myFunction() {
+var a = Math.sqrt(0);
+var b = Math.sqrt(1);
+var c = Math.sqrt(9);
+var d = Math.sqrt(64);
+var e = Math.sqrt(-9);
+
+var x = a + "<br>" + b + "<br>" + c + "<br>" + d + "<br>" + e;
+document.getElementById("demo").innerHTML = x;
+}
+
+function Q_1() {
+var a = Math.sqrt(9);
+var b = Math.sqrt(16);
+var c = a + "<br>" + b;
+document.getElementById("q_1").innerHTML = c;
+}
